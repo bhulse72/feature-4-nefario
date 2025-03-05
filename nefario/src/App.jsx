0d289fb268ab4c41764c904a549be5da9e240fe0
Danny Mendler <odannyboy03@comcast.net>
2025-03-04 20:11:36 -0500

    Env

--- nefario/src/App.jsx
+++ nefario/src/App.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import Components from "./Components/Components.js";
-import * as Env from "./environments.js";
+//import * as Env from "./environments.js";
 import Parse from "parse";
 import "App.css"
+
+const Env = {
+    APPLICATION_ID: "hghyojbRy20HCekyMqxeTtA1dE1jVshTRp4uCeXD",
+    JAVASCRIPT_KEY: "KJgNuLJxlz9rUdtkMIbAOovxdXGOHkuIP3wxracO",
+    SERVER_URL: "https://parseapi.back4app.com/",
+}
 
 Parse.initialize(Env.APPLICATION_ID, Env.JAVASCRIPT_KEY);
 Parse.serverURL = Env.SERVER_URL;
