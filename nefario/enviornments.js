4d9e110e3e8c6fbc9da1be6cdc08f758f265b472
Danny Mendler <odannyboy03@comcast.net>
2025-03-04 20:07:29 -0500

    env.js

--- nefario/enviornments.js
+++ nefario/enviornments.js
@@ -1,6 +1,8 @@
-module.exports = {
+export default function() {
+  return {
     APPLICATION_ID: "hghyojbRy20HCekyMqxeTtA1dE1jVshTRp4uCeXD",
     JAVASCRIPT_KEY: "KJgNuLJxlz9rUdtkMIbAOovxdXGOHkuIP3wxracO",
     SERVER_URL: "https://parseapi.back4app.com/",
   };
+};
   
