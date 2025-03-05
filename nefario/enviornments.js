ebd0525b7295c258a4e2d475779c5bc18fcb61c7
Brandon Hulse <bhulse@brandons-mbp.dhcp.nd.edu>
2025-03-04 20:04:51 -0500

    Copied from kickoff

---
+++ nefario/enviornments.js
@@ -0,0 +1,6 @@
+module.exports = {
+    APPLICATION_ID: "hghyojbRy20HCekyMqxeTtA1dE1jVshTRp4uCeXD",
+    JAVASCRIPT_KEY: "KJgNuLJxlz9rUdtkMIbAOovxdXGOHkuIP3wxracO",
+    SERVER_URL: "https://parseapi.back4app.com/",
+  };
+  
